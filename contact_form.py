9d2c7457b051c53b2f297d7bf2d6d7608f025e40
Denise <denise2001.lara@gmail.com>
2025-02-12 14:44:17 -0300

    Update contact_form.py

--- contact_form.py
+++ contact_form.py
@@ -18,8 +18,8 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465  
 app.config['MAIL_USE_SSL'] = True
-app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')  # Lee la variable del .env
-app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')  # Lee la variable del .env
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')  
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD') 
 app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_USERNAME')
 
 mail = Mail(app)
